e4f4b86cdb81453a753ebe28c6a6bfb2a949b9c3
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-03 19:45:35 +0500

    add new Task1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Пожалуйста введите 2 числа,");
+Console.WriteLine("чтобы узнать какое большее,а какое меньшее.");
+Console.Write("Введите первое число:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+if (num_1 > num_2)
+{
+    Console.WriteLine("Число" + num_1 + "большее");
+    Console.Write("Число" + num_2 + "меньшее");
+}
+else if (num_1 == num_2)
+{
+    Console.WriteLine("Число" + num_1 + "равно числу" + num_2);
+}
+else
+{
+    Console.WriteLine("Число" + num_2 + "большее");
+    Console.Write("Число" + num_1 + "меньшее");
+}
